c3ad57fda8da734586dc1042ed64346594f585f0
Dominik Lau <harek2388@gmail.com>
2024-05-15 19:25:13 +0200

    init commit: setup repo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import BrandNav from './nav/nav';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Reconstruction from './reconstruction/Reconstruction';
+import Generation from './generation/Generation';
+import Description from './description/Description';
+
+function App() {
+  return (
+    <div className="App">
+      <BrandNav />
+      <BrowserRouter>
+      <Routes>
+          <Route index element={<Description/>} />
+          <Route path="/reconstruction" element={<Reconstruction />} />
+          <Route path="/generation" element={<Generation />} />
+          <Route path="*" element={<h1>Invalid page</h1>} />
+      </Routes>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/nav/nav.jsx
@@ -0,0 +1,17 @@
+import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap'
+
+export default function BrandNav(){
+return <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/">3D X-ray angiography</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="reconstruction">Reconstruction</Nav.Link>
+            <Nav.Link href="generation">Generation</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+}
