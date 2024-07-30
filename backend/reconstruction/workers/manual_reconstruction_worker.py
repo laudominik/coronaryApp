575852d8c6f71def88a7dc369a6fa5db5d7cc6bf
Sladamos <sladamo@wp.pl>
2024-07-30 18:52:02 +0200

    Bugfix

--- backend/reconstruction/workers/manual_reconstruction_worker.py
+++ backend/reconstruction/workers/manual_reconstruction_worker.py
@@ -1,5 +1,7 @@
+import json
+
+from django.http import JsonResponse
 from rest_framework.decorators import api_view
-from vessel_tree_generator.module import *
 from reconstruction.manual_parameters_parser import ManualParametersParser
 from xray_angio_3d.manual import find_point
 
